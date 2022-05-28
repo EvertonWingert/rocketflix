e6301adf766550296d53ff45e17e24c985203001
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-05-27 21:29:42 -0300

    update loading states

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,26 @@
 
   const { searchMovieByLetter } = MovieService()
 
+  enum STATES {
+    IDLE,
+    LOADING,
+    LOADED,
+    ERROR,
+  }
+
   const movie = ref({} as Movie)
-  const isLoading = ref<boolean>(false)
+  const state = ref(STATES.IDLE)
   const history = ref<Movie[]>([])
 
   const getRandomMovie = async (query: string) => {
-    isLoading.value = true
+    state.value = STATES.LOADING
     try {
       const response = await searchMovieByLetter(query)
       movie.value = response
       history.value.push(response)
+      state.value = STATES.LOADED
     } catch (error) {
-      console.log(error)
-    } finally {
-      isLoading.value = false
+      state.value = STATES.ERROR
     }
   }
 </script>
@@ -36,19 +42,35 @@
             Não sabe o que assistir?
           </h1>
         </div>
-        <div class="max-w-lg pt-5 mt-5">
-          <div v-if="!movie">
+        <div class="max-w-lg pt-5 mt-5 text-center">
+          <div v-if="state === STATES.IDLE">
+            <span class="text-white">Clique no botão para buscar um filme</span>
+          </div>
+          <div v-if="state === STATES.LOADING">
+            <div class="flex flex-row">
+              <div
+                class="bg-gray-100 opacity-50 animate-pulse w-1/2 h-72"
+              ></div>
+            </div>
+          </div>
+          <div v-if="state === STATES.ERROR">
+            <span class="text-red-300">Falha ao buscar filme</span>
+          </div>
+          <div v-if="state === STATES.LOADED && !movie">
             <span class="text-gray-300">Nenhum filme encontrado</span>
           </div>
-          <MovieCard :movie="movie" />
+          <div v-else-if="state === STATES.LOADED && movie">
+            <MovieCard :movie="movie" />
+          </div>
         </div>
         <button
           @click="getRandomMovie(generateRandomLetter())"
+          :disabled="state === STATES.LOADING"
           type="button"
-          class="inline-flex w-full justify-center items-center px-3 py-2 border border-transparent text-base leading-4 font-medium rounded-md shadow-sm text-[#102d71] bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          class="inline-flex w-full justify-center items-center px-3 py-2 border border-transparent text-base leading-4 font-medium rounded-md shadow-sm text-[#102d71] bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:pointer-events-none disabled:cursor-not-allowed"
         >
           Encontrar filme
-          <Spinner class="ml-2" v-if="isLoading" />
+          <Spinner class="ml-2" v-if="state === STATES.LOADING" />
         </button>
       </div>
       <div>
