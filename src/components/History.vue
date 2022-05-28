ba5209830c6fd5475bc8f38f0f7c78d722cb2d12
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-05-27 21:48:05 -0300

    update history
    add link to movie
    add empty state to history

--- src/components/History.vue
+++ src/components/History.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
   import { Movie } from '../types/movie'
+  import { STATES } from '../types/state'
+  import EmptyMovie from './EmptyMovie.vue'
 
   interface Props {
     movies: Movie[]
@@ -9,12 +11,24 @@
 </script>
 
 <template>
-  <div class="overflow-auto">
+  <div>
     <h1 class="text-white text-2xl pt-5">History</h1>
-    <ul class="list-disc pt-5">
-      <li v-for="movie in movies" class="text-white">
-        {{ movie.title }}
-      </li>
-    </ul>
+    <div
+      class="overflow-auto max-h-56 border border-gray-600 px-2 rounded mt-5"
+    >
+      <div v-if="!movies.length" class="py-5 px-2">
+        <span class="text-gray-300">Nenhum filme no histórico</span>
+      </div>
+      <ul v-else class="list-disc py-3 space-y-2">
+        <li v-for="movie in movies" class="text-white">
+          <a
+            :href="`https://www.themoviedb.org/movie/${movie.id}`"
+            class="hover:underline text-blue-500"
+            target="_blank"
+            >{{ movie.title }}</a
+          >
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
